339a62ad2b94b5846a35b44a297f7f86f2709a6c
Arman Bistibekov <bistibekov.a@gmail.com>
2024-08-30 23:45:31 -0400

    Initial commit

---
+++ src/main/java/kz/tenizcap/emailservice/service/EmailService.java
@@ -0,0 +1,13 @@
+package kz.tenizcap.emailservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender emailMailSender;
+
+
+}
---
+++ src/test/java/kz/tenizcap/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package kz.tenizcap.emailservice;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class EmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertThat(40 + 2).isEqualTo(42);
+    }
+}
